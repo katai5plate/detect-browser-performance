6bb9a7284b4656da219661bb8171d85126dbed37
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2020-05-26 02:35:46 +0900

    Create low-performance-blocker.js

---
+++ low-performance-blocker.js
@@ -0,0 +1,45 @@
+/**
+ * 必須: detect-es.js
+ * ブロック条件:
+ * - ES8 完全対応
+ * - ES9 一部対応 (オブジェクトのspread, for-await-of)
+ * - base64 対応
+ * - WEBGL 完全対応 (experimental-webgl 禁止)
+ * - ローカルファイルアクセス
+ * - Web Audio API 対応
+ */
+
+window.lowPerformanceBlocker = function () {
+  var reasons = [];
+  var tryit = function (reason, code) {
+    try {
+      if (typeof code === "string") {
+        new Function(code);
+        return;
+      }
+      return !!code();
+    } catch (e) {}
+    reasons.push(reason);
+  };
+  tryit("ES8", function () {
+    return detectES(8);
+  });
+  tryit("ES9 Rest/Spread Properties", "(()=>({...[1,2]}))()");
+  tryit("ES9 for-await-of", "async()=>{for await (let x of []) {}}");
+  tryit("base64 conversions", function () {
+    return "atob" in window && "btoa" in window;
+  });
+  tryit("WEBGL (Full support)", function () {
+    return document.createElement("canvas").getContext("webgl");
+  });
+  tryit("Reading local file", function () {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "js/libs/addons/detect-es.js");
+    xhr.overrideMimeType("text/javascript");
+    xhr.send();
+  });
+  tryit("WEB Audio", function () {
+    return "AudioContext" in window || webkitAudioContext in window;
+  });
+  return { result: reasons.length !== 0, reasons: reasons };
+};
